504621688c69f69e96bfbd1d7610d36412835cd5
Ajay263 <junioralexio607@gmail.com>
2025-05-30 19:32:23 +0200

    fix:removed unused output in terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,3 @@ output "ecr_repository_url" {
   value       = aws_ecr_repository.ecr_repo.repository_url
 }
 
-output "s3_bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.movie_bucket.id
-}
